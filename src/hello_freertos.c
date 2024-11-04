f34111ff9f535ee7842368b1cdc924a3933d045f
Muteeb Akram <muteebakram@gmail.com>
2024-11-04 13:38:40 -0700

    lab06 activity 1 priority inversion completed

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -9,46 +9,80 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include <semphr.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-int count = 0;
-bool on = false;
+bool shared_value = false;
+SemaphoreHandle_t xSemaphore;
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
-#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define SUPERVISOR_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
+#define SUBORDINATE_TASK_1_PRIORITY (tskIDLE_PRIORITY + 2UL)
+#define SUBORDINATE_TASK_2_PRIORITY (tskIDLE_PRIORITY + 3UL)
+#define SUPERVISOR_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define SUBORDINATE_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-void blink_task(__unused void *params) {
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
-        vTaskDelay(500);
+void subordinate_task(__unused void *params)
+{
+    while (1)
+    {
+        printf("In the subordinate\n");
+        if (xSemaphoreTake(xSemaphore, 10000) == pdTRUE)
+        {
+            shared_value = !shared_value;
+            printf("Updated semaphore %d\n", shared_value);
+            vTaskDelay(10000);
+            xSemaphoreGive(xSemaphore);
+        }
     }
 }
 
-void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
+void supervisor_task(__unused void *params)
+{
+    // printf("where0...\n");
+
+    // printf("where1...\n");
+
+    // printf("where2...\n");
+    xSemaphore = xSemaphoreCreateBinary();
+    if (xSemaphore == NULL)
+    {
+        printf("Failed to create binary semaphore.\n");
+        return;
+    }
+
+    // Initially set the semaphore to available state.
+    xSemaphoreGive(xSemaphore);
+    // TaskHandle_t subordinate_task1, subordinate_task2;
+
+    printf("Stating low priority...\n");
+    xTaskCreate(subordinate_task, "SubordinateThread2",
+                SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_2_PRIORITY, NULL);
+
+    vTaskDelay(10000);
+
+    printf("Stating high priority...\n");
+    xTaskCreate(subordinate_task, "SubordinateThread1",
+                SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_1_PRIORITY, NULL);
+    while (1)
+    {
+        printf("Hi there!\n");
+        vTaskDelay(1000);
     }
 }
 
-int main( void )
+int main(void)
 {
     stdio_init_all();
-    const char *rtos_name;
-    rtos_name = "FreeRTOS";
-    TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    sleep_ms(5000);
+    printf("Starting...\n");
+
+    // TaskHandle_t supervisor_task;
+    xTaskCreate(supervisor_task, "SupervisorThread",
+                SUPERVISOR_TASK_STACK_SIZE, NULL, SUPERVISOR_TASK_PRIORITY, NULL);
     vTaskStartScheduler();
+
     return 0;
 }
