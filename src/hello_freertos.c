123a32a0b8168af94784e19d0e13e0de82b8afde
Stephen <ehlert.stephenj#gmail.com>
2024-11-06 12:54:23 -0700

    priority inversion works

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -14,7 +14,7 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-
+int x = 0;
 SemaphoreHandle_t xSemaphore;
 
 #define SUPERVISOR_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
@@ -35,12 +35,10 @@ void subordinate_task_low(__unused void *params)
         {
             //Take semaphore, then run busy wait loop to simulate a computation on a shared variable, then give back semaphore
             printf("Starting computation in low subordinate task\n");
-            for (int i = 0; i < 50000; i++)
-            {
-                __nop();
-            }
-            printf("Finished computation giving back semaphore in high subordinate task\n");
+            sleep_ms(10000);
+            printf("Finished computation giving back semaphore in low subordinate task\n");
             xSemaphoreGive(xSemaphore);
+            sleep_ms(10000);
         }
     }
 }
@@ -50,19 +48,11 @@ void subordinate_task_medium(__unused void *params)
 {
     while (1)
     {
-        printf("Starting computation 1 in medium subordinate task\n");
-        //Run busy wait loop to simulate computing something
-        for (int i = 0; i < 50000; i++)
-        {
-            __nop();
-        }
+        
         printf("Starting computation 2 in medium subordinate task\n");
-        for (int i = 0; i < 50000; i++)
-        {
-            __nop();
-        }
+        //Run busy wait loop to simulate computing something
+        sleep_ms(20000);
         printf("Finished computation 2 in medium subordinate task\n");
-        vTaskDelay(5000);
     }
 }
 
@@ -77,10 +67,7 @@ void subordinate_task_high(__unused void *params)
             //Take semaphore, then run busy wait loop to simulate a computation on a shared variable, 
             //then give back semaphore
             printf("Starting computation in high subordinate task\n");
-            for (int i = 0; i < 50000; i++)
-            {
-                __nop();
-            }
+            sleep_ms(5000);
             printf("Finished computation giving back semaphore in high subordinate task\n");
             xSemaphoreGive(xSemaphore);
         }
@@ -103,20 +90,23 @@ void supervisor_task(__unused void *params)
 
     //Start low task and delay to have it take semaphore 
     printf("Stating low priority task\n");
-    xTaskCreate(subordinate_task_low, "SubordinateThread2",
+    xTaskCreate(subordinate_task_low, "SubordinateLow",
                 SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_3_PRIORITY, NULL);
-    vTaskDelay(500);
+    vTaskDelay(1);
 
+    xTaskCreate(subordinate_task_high, "SubordinateHigh",
+                SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_1_PRIORITY, NULL);
+
+    vTaskDelay(1);
     //Start medium task to have it interrupt low task and start computation
     printf("Stating medium priority task\n");
-    xTaskCreate(subordinate_task_medium, "SubordinateThread3",
+    xTaskCreate(subordinate_task_medium, "SubordinateMedium",
                 SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_2_PRIORITY, NULL);
-    vTaskDelay(500);
+    
 
     //Start high task, but it won't be able to do anything since low task has semaphore
-    printf("Stating high priority...\n");
-    xTaskCreate(subordinate_task, "SubordinateThread1",
-                SUBORDINATE_TASK_STACK_SIZE, NULL, SUBORDINATE_TASK_1_PRIORITY, NULL);
+    //printf("Stating high priority...\n");
+
 
     vTaskDelay(100000);
 
